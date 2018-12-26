982161764cc23cf11785f5be3b4c9e300ba2050c
Administrator <549477611@qq.com>
2018-12-26 11:09:49 +0800

    Idworker bug commit.

--- myth-common/src/main/java/org/dromara/myth/common/utils/IdWorkerUtils.java
+++ myth-common/src/main/java/org/dromara/myth/common/utils/IdWorkerUtils.java
@@ -17,44 +17,62 @@
 
 package org.dromara.myth.common.utils;
 
-import java.util.UUID;
+import java.util.Random;
 
 /**
- * snow .
+ * snow flow .
  *
  * @author xiaoyu
  */
 public final class IdWorkerUtils {
 
+    private static final Random RANDOM = new Random();
+
+    private static final long WORKER_ID_BITS = 5L;
+
+    private static final long DATACENTERIDBITS = 5L;
+
+    private static final long MAX_WORKER_ID = ~(-1L << WORKER_ID_BITS);
+
+    private static final long MAX_DATACENTER_ID = ~(-1L << DATACENTERIDBITS);
+
+    private static final long SEQUENCE_BITS = 12L;
+
+    private static final long WORKER_ID_SHIFT = SEQUENCE_BITS;
+
+    private static final long DATACENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS;
+
+    private static final long TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATACENTERIDBITS;
+
+    private static final long SEQUENCE_MASK = ~(-1L << SEQUENCE_BITS);
+
     private static final IdWorkerUtils ID_WORKER_UTILS = new IdWorkerUtils();
-
-    private final long twepoch = 1288834974657L;
-
-    private final long workerIdBits = 5L;
-
-    private final long datacenterIdBits = 5L;
-
-    private final long maxWorkerId = ~(-1L << workerIdBits);
-
-    private final long maxDatacenterId = ~(-1L << datacenterIdBits);
-
-    private final long sequenceBits = 12L;
-
-    private final long workerIdShift = sequenceBits;
-
-    private final long datacenterIdShift = sequenceBits + workerIdBits;
-
-    private final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
-
-    private final long sequenceMask = ~(-1L << sequenceBits);
 
     private long workerId;
 
     private long datacenterId;
 
-    private long sequence = 0L;
+    private long idepoch;
+
+    private long sequence = '0';
 
     private long lastTimestamp = -1L;
+
+    private IdWorkerUtils() {
+        this(RANDOM.nextInt((int) MAX_WORKER_ID), RANDOM.nextInt((int) MAX_DATACENTER_ID), 1288834974657L);
+    }
+
+    private IdWorkerUtils(final long workerId, final long datacenterId, final long idepoch) {
+        if (workerId > MAX_WORKER_ID || workerId < 0) {
+            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", MAX_WORKER_ID));
+        }
+        if (datacenterId > MAX_DATACENTER_ID || datacenterId < 0) {
+            throw new IllegalArgumentException(String.format("datacenter Id can't be greater than %d or less than 0", MAX_DATACENTER_ID));
+        }
+        this.workerId = workerId;
+        this.datacenterId = datacenterId;
+        this.idepoch = idepoch;
+    }
 
     /**
      * Gets instance.
@@ -65,28 +83,13 @@ public final class IdWorkerUtils {
         return ID_WORKER_UTILS;
     }
 
-    private IdWorkerUtils() {
-
-    }
-
-    private IdWorkerUtils(final long workerId, final long datacenterId) {
-        if (workerId > maxWorkerId || workerId < 0) {
-            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
-        }
-        if (datacenterId > maxDatacenterId || datacenterId < 0) {
-            throw new IllegalArgumentException(String.format("datacenter Id can't be greater than %d or less than 0", maxDatacenterId));
-        }
-        this.workerId = workerId;
-        this.datacenterId = datacenterId;
-    }
-
     private synchronized long nextId() {
         long timestamp = timeGen();
         if (timestamp < lastTimestamp) {
-            throw new RuntimeException(String.format("Clock    moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
+            throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
         }
         if (lastTimestamp == timestamp) {
-            sequence = (sequence + 1) & sequenceMask;
+            sequence = (sequence + 1) & SEQUENCE_MASK;
             if (sequence == 0) {
                 timestamp = tilNextMillis(lastTimestamp);
             }
@@ -96,10 +99,12 @@ public final class IdWorkerUtils {
 
         lastTimestamp = timestamp;
 
-        return ((timestamp - twepoch) << timestampLeftShift) | (datacenterId << datacenterIdShift) | (workerId << workerIdShift) | sequence;
+        return ((timestamp - idepoch) << TIMESTAMP_LEFT_SHIFT)
+                | (datacenterId << DATACENTER_ID_SHIFT)
+                | (workerId << WORKER_ID_SHIFT) | sequence;
     }
 
-    private long tilNextMillis(long lastTimestamp) {
+    private long tilNextMillis(final long lastTimestamp) {
         long timestamp = timeGen();
         while (timestamp <= lastTimestamp) {
             timestamp = timeGen();
@@ -117,6 +122,7 @@ public final class IdWorkerUtils {
      * @return the string
      */
     public String createUUID() {
-        return String.valueOf(UUID.randomUUID().hashCode() & 0x7fffffff);
+        return String.valueOf(ID_WORKER_UTILS.nextId());
     }
+
 }
